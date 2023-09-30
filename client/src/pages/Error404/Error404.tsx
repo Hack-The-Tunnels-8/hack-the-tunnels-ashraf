ce310349b57edf72cae9b006a4884ddce438da87
Ashrafzhcet1 <ashrafzhcet@gmail.com>
2023-09-30 13:53:47 -0400

    Add 404 Page addded gif

--- client/src/pages/Error404/Error404.tsx
+++ client/src/pages/Error404/Error404.tsx
@@ -6,6 +6,7 @@ function Error404() {
     return (
         <Page>
 <h3>Page not found</h3>
+<div style={{width:"480px"}}><iframe allow="fullscreen" frameBorder="0" height="225" src="https://giphy.com/embed/d1pItNWEYFHlVVyNqe/video" width="480"></iframe></div>
         </Page>
     );
 }
