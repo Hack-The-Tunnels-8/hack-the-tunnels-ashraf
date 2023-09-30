cdb2d1f33f4eb35e83af1cde193ef8613a133ff4
Shah-Ban014 <shah.bano@onyxglobal.io>
2023-09-30 16:45:26 -0400

    Improve Product Preview Card Component

--- client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
+++ client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
@@ -10,9 +10,9 @@ interface Props {
 function ProductPreviewcard({ title, description, price, imageUrl }: Props) {
   return (
     <div className="product-preview-card">
-      <img className="product-preview-card__image" src={imageUrl} alt={title} />
-      <h3 className="product-preview-card__title">{title}</h3>
-      <p className="product-preview-card__description">{description}</p>
+      <img className="product-preview-card__image" src="https://www.gstatic.com/webp/gallery3/1.png" alt={title} />
+      <h3 className="product-preview-card__title">Flower</h3>
+      <p className="product-preview-card__description">Artificial Flower</p>
       <p className="product-preview-card__price">${price}</p>
     </div>
   );
