4ff4ad18584dc5acf90af41aa47972edbe764aaa
Shah-Ban014 <shah.bano@onyxglobal.io>
2023-09-30 17:03:35 -0400

    Add Login Page Form

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -8,13 +8,16 @@ function Login() {
   const [message, setMessage] = useState(null);
   const { loggedIn, login } = useAccountContext();
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
 
   const attemptLogin = async () => {
     try {
       const message = await login("admin@email.com", "password");
       setMessage(message);
     } catch (error) {
-      console.log(error);
+      setMessage('Invalid email or password');
     }
   };
 
@@ -25,16 +28,28 @@ function Login() {
   }, [loggedIn, navigate]);
 
   return (
-    <Page>
-      <div className="login-page">
-        <h1>Login</h1>
-        <button onClick={() => attemptLogin()}>
-          Login (as user set in code)
-        </button>
-        {message && <p>{message}</p>}
+    <div>
+      <h2>Login</h2>
+      <div>
+        <label>Email:</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
-    </Page>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={attemptLogin}>Login</button>
+      <p>{message}</p>
+    </div>
   );
-}
+};
 
 export default Login;
